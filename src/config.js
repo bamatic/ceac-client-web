b161f48549aed7793f91830aa3289b0ad094cd20
victor <victorzugadi@bamatic.com>
2023-12-12 08:32:39 +0000

    paginator config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_URL = 'http://localhost:8080';
 
-export const VENDOR_NAME = 'BAMATIC';
+export const VENDOR_NAME = 'BIO FOOD';
 export const VENDOR_ADDRESS = 'Calle IRALA 27,  48993, BILBO'
-export const PRODUCTS_PAR_PAGE = 2;
+export const PRODUCTS_PAR_PAGE = 20;
 export const LOGIN_URL = '/login/';
